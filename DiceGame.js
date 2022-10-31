b59664d240211e0374fb6605043aca17c7973c66
Darshan <darshankollam8@gmail.com>
2022-10-31 13:02:48 +0530

    Final File

---
+++ DiceGame.js
@@ -0,0 +1,18 @@
+let counting = () => {
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    var randomDiceImage = "assets/dice" + randomNumber1 + ".png";
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    var randomDiceImage2 = "assets/dice" + randomNumber2 + ".png";
+
+    document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage);
+    document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
+
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = "Player 1 Wins";
+    } else if (randomNumber1 < randomNumber2) {
+        document.querySelector("h1").innerHTML = "Player 2 Wins";
+    } else {
+        document.querySelector("h1").innerHTML = "Draw";
+    }
+    let h = document.getElementsByClassName(".btn");
+};
